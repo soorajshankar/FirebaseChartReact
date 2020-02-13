e3df062cff09d8492e144f48317a54789a2f2e62
soorajshankar <soorajshankar@gmail.com>
2020-02-13 11:53:17 +0530

    Ping added :)

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import "./App.css";
 // import StateExample from "./components/StateExample";
 // import FCDemo from "./components/FCDemo";
 import Table from "./components/Table";
-import { init, analytics } from "./config/firebase";
+import { init, analytics, FBDatabase } from "./config/firebase";
 import ChartComponent from "./components/ChartComponent";
+import moment from "moment";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -16,12 +17,18 @@ function App() {
     analytics();
     setLoading(false);
   }, []);
+  const onClick = () => {
+    FBDatabase()
+      .ref("ping")
+      .set(moment().unix());
+  };
   return (
     <div className="App">
       {/* <header className="App-header"> */}
       {!loading && <ChartComponent {...{ type: "co2" }} />}
       {!loading && <ChartComponent {...{ type: "humidity" }} />}
       {!loading && <ChartComponent {...{ type: "temperature" }} />}
+      <button onClick={onClick}>Ping</button>
       {/* <Table /> */}
       {/* </header> */}
     </div>
